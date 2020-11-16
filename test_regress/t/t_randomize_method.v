f954dcd639151af7c802d480062e1fa69acdf38a
Krzysztof Bieganski <kbieganski@antmicro.com>
2020-11-16 14:51:59 +0100

    Add randomize() method test

--- test_regress/t/t_randomize_method.v
+++ test_regress/t/t_randomize_method.v
@@ -13,23 +13,48 @@ class Inner;
    rand logic[3:0] c;
    rand logic[11:0] d;
    int e;
+
+   function new;
+      a = 0;
+      b = 0;
+      c = 0;
+      d = 0;
+      e = 0;
+   endfunction
+
 endclass
 
 class DerivedA extends Base;
    rand Inner i;
    rand int j;
    int k;
+
+function new;
+      i = new;
+      j = 0;
+      k = 0;
+   endfunction
+
 endclass
 
 class DerivedB extends Base;
+   logic[63:0] v;
    rand logic[63:0] w;
    rand logic[47:0] x;
    rand logic[31:0] y;
    rand logic[23:0] z;
-   logic[63:0] v;
+
+   function new;
+      v = 0;
+      w = 0;
+      x = 0;
+      y = 0;
+      z = 0;
+   endfunction
+
 endclass
 
-module top(input clk);
+module t (/*AUTOARG*/);
    DerivedA derivedA;
    DerivedB derivedB;
    Base base;
@@ -37,7 +62,6 @@ module top(input clk);
    initial begin
       int rand_result;
       derivedA = new;
-      derivedA.i = new;
       derivedB = new;
       base = derivedA;
       rand_result = base.randomize();
@@ -49,11 +73,11 @@ module top(input clk);
       if (derivedA.i.e != 0) $stop;
       if (derivedA.j == 0) $stop;
       if (derivedA.k != 0) $stop;
+      if (derivedB.v != 0) $stop;
       if (derivedB.w == 0) $stop;
       if (derivedB.x == 0) $stop;
       if (derivedB.y == 0) $stop;
       if (derivedB.z == 0) $stop;
-      if (derivedB.v != 0) $stop;
       $write("*-* All Finished *-*\n");
       $finish;
    end
