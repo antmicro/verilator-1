c1e5af630b50b4115d1fbf9f49ea984e93ef658b
Tomasz Gorochowik <tgorochowik@antmicro.com>
2020-12-11 14:35:48 +0100

    Don't mark settle functions as oneshot

--- src/V3Order.cpp
+++ src/V3Order.cpp
@@ -1783,9 +1783,11 @@ AstActive* OrderVisitor::processMoveOneLogic(const OrderLogicVertex* lvertexp,
             newFuncpr->argTypes(EmitCBaseVisitor::symClassVar());
             newFuncpr->symProlog(true);
             newStmtsr = 0;
+            if (domainp->hasInitial()) {
+                newFuncpr->oneshot(true);
+            }
             if (domainp->hasInitial() || domainp->hasSettle()) {
                 newFuncpr->slow(true);
-                newFuncpr->oneshot(true);
             }
             scopep->addActivep(newFuncpr);
             // Where will we be adding the call?
