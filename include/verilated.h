d8e2268927247742037a89aa8936e8b9b5234d2c
Tomasz Gorochowik <tgorochowik@antmicro.com>
2020-12-14 14:40:04 +0100

    Lock MonitoredValue mutex before casting to a primitive type

--- include/verilated.h
+++ include/verilated.h
@@ -345,6 +345,7 @@ class MonitoredValue : public MonitoredValueBase {
         }
 
         operator T() const {
+            std::unique_lock<std::mutex> lck(mtx);
             return (T)value;
         }
 
@@ -473,7 +474,7 @@ class MonitoredValue : public MonitoredValueBase {
     private:
         T value;
 
-        std::mutex mtx;
+        mutable std::mutex mtx;
 
         std::vector<Promise<T>*> promises;
 
