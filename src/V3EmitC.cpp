52a5afa90406bc798af27ef27572a429acba9e10
Krzysztof Bieganski <kbieganski@antmicro.com>
2021-01-26 13:39:09 +0100

    Handle delayed assignment to select node

--- src/V3EmitC.cpp
+++ src/V3EmitC.cpp
@@ -377,7 +377,8 @@ public:
     virtual void visit_assigndly(AstNodeAssign *nodep, bool continuous) {
         if (VN_IS(nodep->lhsp(), VarRef) ||
             VN_IS(nodep->lhsp(), ArraySel) ||
-            VN_IS(nodep->lhsp(), WordSel)) {
+            VN_IS(nodep->lhsp(), WordSel) ||
+            VN_IS(nodep->lhsp(), Sel)) {
             puts("verilated_nba_ctrl.schedule(&");
             iterateAndNextNull(nodep->lhsp());
             puts(", ");
