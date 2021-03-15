cbba3c21a0ac530e852d0affc698b8ef923d5a4f
Krzysztof Bieganski <kbieganski@antmicro.com>
2021-03-15 13:07:15 +0100

    Fix duplicate func/task declarations

--- src/V3LinkDot.cpp
+++ src/V3LinkDot.cpp
@@ -871,7 +871,7 @@ class LinkDotFindVisitor final : public AstNVisitor {
     virtual void visit(AstScope* nodep) override {
         UASSERT_OBJ(m_statep->forScopeCreation(), nodep,
                     "Scopes should only exist right after V3Scope");
-        iterateChildren(nodep);
+        // Ignored.  Processed in next step
     }
     virtual void visit(AstTopScope* nodep) VL_OVERRIDE { iterateChildren(nodep); }
     virtual void visit(AstCell* nodep) override {
