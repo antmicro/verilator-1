aa18c670c6955903be1819e46ba83440dcad0c53
Pawel Sagan <psagan@antmicro.com>
2021-03-25 21:54:08 +0100

    fixing duplication of combo blocks

--- src/V3ActiveTop.cpp
+++ src/V3ActiveTop.cpp
@@ -79,14 +79,14 @@ private:
             return;
         }
         // Copy combo tree to settlement tree with duplicated statements
-        if (sensesp->hasCombo()) {
-            AstSenTree* newsentreep = new AstSenTree(
-                nodep->fileline(), new AstSenItem(nodep->fileline(), AstSenItem::Settle()));
-            AstActive* newp = new AstActive(nodep->fileline(), "settle", newsentreep);
-            newp->sensesStorep(newsentreep);
-            if (nodep->stmtsp()) newp->addStmtsp(nodep->stmtsp()->cloneTree(true));
-            nodep->addNextHere(newp);
-        }
+       // if (sensesp->hasCombo()) {
+       //     AstSenTree* newsentreep = new AstSenTree(
+       //         nodep->fileline(), new AstSenItem(nodep->fileline(), AstSenItem::Settle()));
+       //     AstActive* newp = new AstActive(nodep->fileline(), "settle", newsentreep);
+       //     newp->sensesStorep(newsentreep);
+       //     if (nodep->stmtsp()) newp->addStmtsp(nodep->stmtsp()->cloneTree(true));
+       //     nodep->addNextHere(newp);
+       // }
         // Move the SENTREE for each active up to the global level.
         // This way we'll easily see what clock domains are identical
         AstSenTree* wantp = m_finder.getSenTree(sensesp);
